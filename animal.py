58779283880b26803ff5d3f137da5f68fd3c18f3
Diego Medina <medinadiego123@yahoo.com>
2024-11-13 19:09:47 -0500

    Add Animal class with species and age attributes

---
+++ animal.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, species, age):
+        self.species = species
+        self.age = age
+
+my_animal = Animal("Lion", 3)
+
+print(my_animal.species)  
+print(my_animal.age)      

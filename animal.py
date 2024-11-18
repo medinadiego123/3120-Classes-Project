761d5d1e2b778e177f1d43098324b12be619b6c6
Diego Medina <medinadiego123@yahoo.com>
2024-11-18 18:42:45 -0500

    Update animal.py with new base class and attributes for inheritance

--- animal.py
+++ animal.py
@@ -1,9 +1,24 @@
 class Animal:
-    def __init__(self, species, age):
+    def __init__(self, species, age, color, habitat):
         self.species = species
         self.age = age
+        self.color = color
+        self.habitat = habitat
 
-my_animal = Animal("Lion", 3)
+    def sound(self):
+        return f"The {self.species} makes a sound."
 
-print(my_animal.species)  
-print(my_animal.age)      
+    def move(self):
+        return f"The {self.species} is moving."
+
+    def eat(self):
+        return f"The {self.species} is eating."
+
+my_animal = Animal("Lion", 3, "Golden", "Savanna")
+print("Species:",my_animal.species)   
+print("Age:",my_animal.age)      
+print("Color:",my_animal.color)    
+print("Habitat:",my_animal.habitat)   
+print(my_animal.sound())  
+print(my_animal.move())        
+print(my_animal.eat())          

42a9998d7ca23957550f28a15ef451211e994c6a
Yongxin <91099716+Yongxin4869@users.noreply.github.com>
2024-11-20 19:04:47 -0500

    Create bird.py

---
+++ bird.py
@@ -0,0 +1,9 @@
+class Bird(Animal):
+    def  __init__(self, age, color, type,  breed):
+         super().__init__("Bird", age, color, "Wild") 
+
+    def move(self):
+         return "The Bird is flying."
+
+    def sound(self):
+        return "The Bird chirps."
